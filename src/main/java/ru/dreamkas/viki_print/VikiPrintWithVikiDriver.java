4e2ae285aaa8bfa689c2858ea17af2a56905c7bb
k.lesnyak <k.lesnyak@dreamkas.ru>
2023-10-20 18:30:43 +0300

    #22283455 - Добавит тесты для VikiPrintWithVikiDriver + рефакторинг.

---
+++ src/main/java/ru/dreamkas/viki_print/VikiPrintWithVikiDriver.java
@@ -0,0 +1,314 @@
+package ru.dreamkas.viki_print;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import com.sun.deploy.util.Waiter;
+
+import javafx.util.Duration;
+
+public class VikiPrintWithVikiDriver {
+    public static final Integer TCP_PORT = 50003;
+    public static final String LOCAL_IP = "127.0.0.1";
+    private static final Pattern LOG_PATTERN = Pattern.compile("\\p{Print}");
+    private static final Charset ENCODING = Charset.forName("cp866");
+    private static final byte ENQ = 0x05;
+    private static final byte ACK = 0x06;
+    private static final char STX = 0x02;
+    private static final char ETX = 0x03;
+    private static final char FS = 0x1C;
+    private static final String PASSWORD = "PIRI";
+    private static int PACKET_ID = 0x20;
+
+    public static void main(String[] args) throws Exception {
+        Socket vikiDriverSocket = new Socket();
+        vikiDriverSocket.connect(new InetSocketAddress(LOCAL_IP, TCP_PORT), 3000);
+        InputStream in = vikiDriverSocket.getInputStream();
+        OutputStream out = vikiDriverSocket.getOutputStream();
+
+        try {
+
+            System.out.println("Проверка связи с ККТ");
+            checkConnection(out, in);
+
+            Object[] responseData;
+
+            System.out.println("Запрос состояния печатающего устройства");
+            responseData = executeCommand(out, in, 0x04);
+            System.out.printf("Статус печатающего устройства: %s%n", responseData[0]);
+            System.out.println();
+
+            System.out.println("Чтение даты/времени ККТ");
+            responseData = executeCommand(out, in, 0x13);
+            System.out.printf("Дата: %s%n", responseData[0]);
+            System.out.printf("Время: %s%n", responseData[1]);
+            System.out.println();
+
+            System.out.println("Запрос флагов статуса ККТ");
+            responseData = executeCommand(out, in, 0x00);
+            int fatalStatus = Integer.parseInt((String) responseData[0]);
+            int status = Integer.parseInt((String) responseData[1]);
+            int docStatus = Integer.parseInt((String) responseData[2]);
+            System.out.printf("Статус фатального состояния ККТ: %s%n", fatalStatus);
+            System.out.printf("Статус текущих флагов ККТ: %s%n", status);
+            System.out.printf("Статус документа: %s%n", docStatus);
+            if ((status & (1L)) != 0) { // Не выполнена команда “Начало работы”
+                LocalDateTime now = LocalDateTime.now();
+                String date = now.format(DateTimeFormatter.ofPattern("ddMMyy"));
+                String time = now.format(DateTimeFormatter.ofPattern("HHmmss"));
+                VikiPrintWithVikiDriver.executeCommand(out, in, 0x10, date, time); // Начало работы с ККТ (0x10)
+            }
+            if ((docStatus & 0x1F) != 0) { // Открыт документ
+                VikiPrintWithVikiDriver.executeCommand(out, in, 0x32); // Аннулировать документ (0x32)
+            }
+            if ((status & (1L << 2)) == 0) { // Смена не открыта
+                VikiPrintWithVikiDriver.executeCommand(out, in, 0x23, "Администратор"); // Открыть смену (0x23)
+            } else if ((status & (1L << 3)) != 0) { // 24 часа истекли
+                VikiPrintWithVikiDriver.executeCommand(out, in, 0x21, "Администратор"); // Сформировать отчет о закрытии смены (0x21)
+                VikiPrintWithVikiDriver.executeCommand(out, in, 0x23, "Администратор"); // Открыть смену (0x23)
+            }
+
+            System.out.println();
+
+            System.out.println("Запрос сведений о ККТ");
+            responseData = executeCommand(out, in, 0x02, 3);
+            System.out.printf("ИНН: %s%n", responseData[1]);
+            responseData = executeCommand(out, in, 0x02, 1);
+            System.out.printf("Заводской номер ККТ: %s%n", responseData[1]);
+            System.out.println();
+
+            System.out.println("Печать сервисного чека");
+            executeCommand(out, in, 0x30, 1, 1);
+            executeCommand(out, in, 0x40, "Текст");
+            executeCommand(out, in, 0x31, 2);
+            System.out.println();
+
+            System.out.println("Продажа штучного и весового товара (Обычный режим формирования документа)");
+            executeCommand(out, in, 0x30, 2, 1, "Петров", "", 0, "");
+            executeCommand(out, in, 0x42, "Сахар", "", 1, 100, 4, "", "", "");
+            executeCommandPacket(out, in, 0x42, "Сахар", "", "1.111", "100", "4", "", "", "", "11");
+            executeCommand(out, in, 0x44);
+            executeCommand(out, in, 0x47, 0, 1000);
+            responseData = executeCommand(out, in, 0x31, 2);
+            System.out.printf("ФД: %s%n", responseData[3]);
+            System.out.printf("ФП: %s%n", responseData[4]);
+            System.out.printf("Номер смены: %s%n", responseData[5]);
+            System.out.printf("Номер документа в смене: %s%n", responseData[6]);
+            System.out.printf("Дата документа: %s%n", responseData[7]);
+            System.out.printf("Время документа: %s%n", responseData[8]);
+            System.out.println();
+
+            System.out.println("Продажа штучного и весового товара (Пакетный режим формирования документа)");
+            executeCommandPacket(out, in, 0x30, 2 | 16, 1, "Петров", "", 0, "");
+            executeCommandPacket(out, in, 0x42, "Сахар", "", 1, 100, 4, "", "", "");
+            executeCommandPacket(out, in, 0x42, "Сахар", "", "1.111", "100", "4", "", "", "", "11");
+            executeCommandPacket(out, in, 0x44);
+            executeCommandPacket(out, in, 0x47, 0, 1000);
+            responseData = executeCommand(out, in, 0x31, 2);
+            System.out.printf("ФД: %s%n", responseData[3]);
+            System.out.printf("ФП: %s%n", responseData[4]);
+            System.out.printf("Номер смены: %s%n", responseData[5]);
+            System.out.printf("Номер документа в смене: %s%n", responseData[6]);
+            System.out.printf("Дата документа: %s%n", responseData[7]);
+            System.out.printf("Время документа: %s%n", responseData[8]);
+            System.out.println();
+
+//          Закрытие архива ФН
+//          responseData = executeCommand(out, in, 0x71, "Петров");
+//          System.out.printf("ФД: %s%n", responseData[0]);
+//          System.out.printf("ФП: %s%n", responseData[1]);
+//          System.out.printf("Дата документа: %s%n", responseData[2]);
+//          System.out.printf("Время документа: %s%n", responseData[3]);
+
+        } finally {
+            vikiDriverSocket.close();
+        }
+    }
+
+    public static void checkConnection(OutputStream out, InputStream in) throws Exception {
+        out.write(ENQ);
+        System.out.printf("==> %s%n", ENQ);
+        while (!(in.available() > 0)) {
+            Thread.yield();
+        }
+        int i = 0;
+        byte[] bytes = new byte[0];
+        while ((i = in.available()) > 0) {
+            bytes = new byte[i];
+            in.read(bytes);
+        }
+
+        System.out.printf("<~~ %s%n", toString(bytes));
+        for (byte[] response : splitPackets(bytes)) {
+            System.out.printf("<== %s%n", toString(response));
+            if (response[0] != ACK) {
+                parseResponse(response);
+            }
+        }
+    }
+
+    //Синхронная работа
+    public static Object[] executeCommand(OutputStream out, InputStream in, int command, Object... parameters) throws Exception {
+        return execute(out, in, command, false, parameters);
+    }
+
+    //Пакетная работа
+    public static void executeCommandPacket(OutputStream out, InputStream in, int command, Object... parameters) throws Exception {
+        execute(out, in, command, true, parameters);
+    }
+
+    //Отправка пакета в порт принтера, получение и разбор ответа
+    private static Object[] execute(OutputStream out, InputStream in, int command, boolean packetMode, Object... parameters) throws Exception {
+        Object[] result = new Object[0];
+
+        byte[] request = makeRequest(command, parameters);
+        System.out.printf("==> %s%n", toString(request));
+        out.write(request);
+        if (packetMode) {
+            return result;
+        }
+        int responsePacketId = 0;
+        while (responsePacketId < PACKET_ID - 1) {
+            while (!(in.available() > 0)) {
+                Thread.yield();
+            }
+            int i = 0;
+            byte[] bytes = new byte[0];
+            while ((i = in.available()) > 0) {
+                bytes = new byte[i];
+                in.read(bytes);
+            }
+
+            System.out.printf("<~~ %s%n", toString(bytes));
+            for (byte[] response : splitPackets(bytes)) {
+                System.out.printf("<== %s%n", toString(response));
+                Object[] responseData = parseResponse(response);
+                responsePacketId = (int) responseData[0];
+                int errorCode = (int) responseData[2];
+                if (errorCode != 0) {
+                    if (errorCode == 0x01 || errorCode == 0x03) {
+                        throw new Exception((String) executeCommand(out, in, 0x06, 1)[2]);
+                    }
+                    throw new Exception(String.format("Ошибка 0x%s", toHexString(errorCode)));
+                }
+
+                result = new Object[responseData.length - 4];
+                System.arraycopy(responseData, 3, result, 0, responseData.length - 4);
+            }
+        }
+        return result;
+    }
+
+    //Разбиение полученных байтов по пакетам
+    static List<byte[]> splitPackets(byte[] response) {
+        List<byte[]> result = new ArrayList<>();
+        List<Byte> part = new ArrayList<>();
+        part.add(response[0]);
+        for (int i = 1; i < response.length; i++) {
+            if (response[i] != STX) {
+                part.add(response[i]);
+            }
+            if (response[i] == STX || i == response.length - 1) {
+                result.add(ArrayUtils.toPrimitive(part.toArray(new Byte[0])));
+                part.clear();
+                part.add(response[i]);
+            }
+        }
+        return result;
+    }
+
+    //Формирование пакета команды со стороны КП
+    private static byte[] makeRequest(int command, Object... parameters) {
+        StringBuilder strPacket = new StringBuilder()
+            .append(STX)                                    // STX
+            .append(PASSWORD)                               // Пароль связи
+            .append((char) PACKET_ID++)                     // ID пакета
+            .append(toHexString(command));                  // Код команды
+        if (parameters.length == 0) {                       //
+            strPacket.append(FS);                           //
+        } else {                                            //
+            for (Object param : parameters) {               //
+                strPacket.append(param).append(FS);         // Данные
+            }                                               //
+        }                                                   //
+        strPacket.append(ETX);                              // ETX
+        int crc = calculateCrc(strPacket.toString());
+        strPacket.append(toHexString(crc));                 // CRC
+
+        return strPacket
+            .toString()
+            .getBytes(ENCODING);
+    }
+
+    //Парсинг пакета ответа со стороны КП
+    static Object[] parseResponse(byte[] response) throws Exception {
+        String data = new String(response, ENCODING);
+        int packetId = data.charAt(1);                              // ID пакета
+        int command = Integer.parseInt(data.substring(2, 4), 16);   // Код команды
+        int errorCode = Integer.parseInt(data.substring(4, 6), 16); // Код ошибки
+        String dataPart = data.substring(6, data.indexOf(ETX));     // Данные
+        String dataForCRCPart = data.substring(0, data.indexOf(ETX) + 1);
+        String crcPart = data.substring(data.indexOf(ETX) + 1);
+        int crc = Integer.parseInt(crcPart, 16);
+        if (crc != calculateCrc(dataForCRCPart)) {
+            throw new Exception("Wrong CRC");
+        }
+
+        Object[] dataArray = Arrays.stream(dataPart.split(String.valueOf(FS))).toArray();
+        Object[] result = new Object[3 + dataArray.length + 1];
+        result[0] = packetId;
+        result[1] = command;
+        result[2] = errorCode;
+        System.arraycopy(dataArray, 0, result, 3, dataArray.length);
+        result[result.length - 1] = crc;
+        return result;
+    }
+
+    //Расчет контрольной суммы пакета
+    private static int calculateCrc(String string) {
+        byte[] rawPacket = string.getBytes(ENCODING);
+        int crc = 0;
+        for (int i = 1; i < rawPacket.length; i++) {
+            crc = (crc ^ rawPacket[i]) & 0xFF;
+        }
+        return crc;
+    }
+
+    //Красивая запись данных пакета для логирования
+    private static String toString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : new String(bytes, ENCODING).toCharArray()) {
+            if (c > 255 || LOG_PATTERN.matcher(String.valueOf(c)).matches()) {
+                sb.append(c);
+            } else {
+                sb.append('{').append(toHexString(c)).append('}');
+            }
+        }
+        return sb.toString();
+    }
+
+    //Понятное преобразование 16-тиричного представления числа
+    private static String toHexString(int value) {
+        return StringUtils.leftPad(Integer.toHexString(value & 0xFF).toUpperCase(), 2, '0');
+    }
+}
+
---
+++ src/test/java/VikiPrintWithVikiDriverTest.java
@@ -0,0 +1,186 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import jssc.SerialPortException;
+import ru.dreamkas.viki_print.VikiPrint;
+import ru.dreamkas.viki_print.VikiPrintWithVikiDriver;
+
+public class VikiPrintWithVikiDriverTest {
+
+    private static InputStream in;
+    private static OutputStream out;
+    private static Socket vikiDriverSocket;
+    @BeforeAll
+    public static void setup() throws Exception {
+        vikiDriverSocket = new Socket();
+        vikiDriverSocket.connect(new InetSocketAddress("127.0.01", 50003), 3000);
+        in = vikiDriverSocket.getInputStream();
+        out = vikiDriverSocket.getOutputStream();
+        VikiPrintWithVikiDriver.checkConnection(out, in);
+    }
+
+    @BeforeEach
+    public void prepare() throws Exception {
+        Object[] flags = VikiPrintWithVikiDriver.executeCommand(out, in, 0x00);
+        int status = Integer.parseInt((String) flags[1]);
+        int docStatus = Integer.parseInt((String) flags[2]);
+        if ((status & (1L)) != 0) { // Не выполнена команда “Начало работы”
+            LocalDateTime now = LocalDateTime.now();
+            String date = now.format(DateTimeFormatter.ofPattern("ddMMyy"));
+            String time = now.format(DateTimeFormatter.ofPattern("HHmmss"));
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x10, date, time); // Начало работы с ККТ (0x10)
+        }
+        if ((docStatus & 0x1F) != 0) { // Открыт документ
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x32); // Аннулировать документ (0x32)
+        }
+        if ((status & (1L << 2)) == 0) { // Смена не открыта
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x23, "Администратор"); // Открыть смену (0x23)
+        } else if ((status & (1L << 3)) != 0) { // 24 часа истекли
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x21, "Администратор"); // Сформировать отчет о закрытии смены (0x21)
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x23, "Администратор"); // Открыть смену (0x23)
+        }
+    }
+
+    @AfterAll
+    public static void finalisation() throws SerialPortException, IOException {
+        vikiDriverSocket.close();
+    }
+
+    @Test
+    @DisplayName("Закрытие / открытие кассовой смены")
+    public void testShiftCloseAndOpen() throws Exception {
+        Object[] flags = VikiPrintWithVikiDriver.executeCommand(out, in, 0x00);
+        int status = Integer.parseInt((String) flags[1]);
+        if ((status & (1L << 2)) != 0) {
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x21, "Администратор"); // Сформировать отчет о закрытии смены (0x21)
+        } else {
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x23, "Администратор"); // Открыть смену (0x23)
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x21, "Администратор"); // Сформировать отчет о закрытии смены (0x21)
+        }
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x23, "Администратор"); // Открыть смену (0x23)
+    }
+
+    @Test
+    @DisplayName("Перерегистрация ККТ без замены ФН")
+    public void testRegistrationKKT() throws Exception {
+        //Проверим, что смена не закрыта
+        Object[] flags = VikiPrintWithVikiDriver.executeCommand(out, in, 0x00);
+        int status = Integer.parseInt((String) flags[1]);
+        if ((status & (1L << 2)) != 0) {
+            //Закроем смену
+            VikiPrintWithVikiDriver.executeCommand(out, in, 0x21, "Администратор"); // Сформировать отчет о закрытии смены (0x21)
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        String date = now.format(DateTimeFormatter.ofPattern("ddMMyy"));
+        String time = now.format(DateTimeFormatter.ofPattern("HHmmss"));
+        //Выполним перерегистрацию ККТ без замены ФН
+        Object[] response = VikiPrintWithVikiDriver.executeCommand(out, in, 0x60,
+            0,
+            "1000000000008261",
+            "1122334455",
+            1,
+            0,
+            "Петров",
+            date,
+            time,
+            "            НАЗВАНИЕ ОРГАНИЗАЦИИ            ",
+            "",
+            "             АДРЕС ОРГАНИЗАЦИИ              ",
+            "",
+            "второй этаж",
+            "",
+            "Такском ОФД",
+            "7704211201",
+            "ofd-receipts@dreamkas.ru",
+            "www.nalog.gov.ru",
+            2,
+            4
+        ); // Регистрация ККТ
+
+        Assertions.assertNotEquals(0, response.length, "Не получен номер ФД");
+        Assertions.assertNotNull(response[0], "Не получен номер ФД");
+        Assertions.assertNotNull(response[1], "Не получена ФП");
+        date = (String) response[2];
+        time = (String) response[3];
+        LocalDateTime regDate = LocalDateTime.parse(date + time, DateTimeFormatter.ofPattern("ddMMyyHHmmss"));
+        Assertions.assertTrue(ChronoUnit.MINUTES.between(regDate, LocalDateTime.now()) < 5);
+    }
+
+    @Test
+    @DisplayName("Формирование чека в пакетном режиме")
+    public void testPurchaseInPacketMode() throws Exception {
+        // Запрос проверки КМ в ФН
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x79, 3);
+        Object[] data = VikiPrintWithVikiDriver.executeCommand(out, in, 0x79, 1, "OTc4MDIwMTM3OTYy", 0, 2, 900, 10, 1);
+        int tag2106 = Integer.parseInt((String) data[1]);
+
+        // Подтверждение добавления КМ в чек
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x79, 2, 1, "", "", "", "", "");
+
+        VikiPrintWithVikiDriver.executeCommandPacket(out, in, 0x30, 2 | 16, 1, "Петров", "", 0, "");
+        VikiPrintWithVikiDriver.executeCommandPacket(out, in, 0x79, 15, "OTc4MDIwMTM3OTYy", 2, 0, tag2106, 10, "");
+        VikiPrintWithVikiDriver.executeCommandPacket(out, in, 0x42, "Маркированный товар", "", 1, 100, 0, "", "", "", 10, "", 4, 4, "", "", "");
+        VikiPrintWithVikiDriver.executeCommandPacket(out, in, 0x42, "Штучный товар", "", 1, 100, 4, "", "", "");
+        VikiPrintWithVikiDriver.executeCommandPacket(out, in, 0x42, "Весовой товар", "", 1.111, 100, 4, "", "", "", 11);
+        VikiPrintWithVikiDriver.executeCommandPacket(out, in, 0x44);
+        VikiPrintWithVikiDriver.executeCommandPacket(out, in, 0x47, 0, 1000);
+        Object[] response = VikiPrintWithVikiDriver.executeCommand(out, in, 0x31, "2");
+        Assertions.assertNotEquals(0, response.length, "Не получен номер ФД");
+        Assertions.assertNotNull(response[3], "Не получен номер ФД");
+        Assertions.assertNotNull(response[4], "Не получена ФП");
+        Assertions.assertNotNull(response[5], "Не получена номер смены");
+        Assertions.assertNotNull(response[6], "Не получен номер документа в смене");
+        Assertions.assertNotNull(response[7], "Не получен дата документа");
+        Assertions.assertNotNull(response[8], "Не получен время документа");
+        String date = (String) response[7];
+        String time = (String) response[8];
+        LocalDateTime regDate = LocalDateTime.parse(date + time, DateTimeFormatter.ofPattern("ddMMyyHHmmss"));
+        Assertions.assertTrue(ChronoUnit.MINUTES.between(regDate, LocalDateTime.now()) < 5);
+    }
+
+    @Test
+    @DisplayName("Формирование чека в синхронном режиме")
+    public void testPurchaseInRegularMode() throws Exception {
+        // Запрос проверки КМ в ФН
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x79, 3);
+        Object[] data = VikiPrintWithVikiDriver.executeCommand(out, in, 0x79, 1, "OTc4MDIwMTM3OTYy", 0, 2, 900, 10, 1);
+        int tag2106 = Integer.parseInt((String) data[1]);
+
+        // Подтверждение добавления КМ в чек
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x79, 2, 1, "", "", "", "", "");
+
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x30, 2, 1, "Петров", "", 0, "");
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x79, 15, "OTc4MDIwMTM3OTYy", 2, 0, tag2106, 10, "");
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x42, "Маркированный товар", "", 1, 100, 0, "", "", "", 10, "", 4, 4, "", "", "");
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x42, "Штучный товар", "", 1, 100, 4, "", "", "");
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x42, "Весовой товар", "", 1.111, 100, 4, "", "", "", 11);
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x44);
+        VikiPrintWithVikiDriver.executeCommand(out, in, 0x47, 0, 1000.0);
+        Object[] response = VikiPrintWithVikiDriver.executeCommand(out, in, 0x31, 2);
+        Assertions.assertNotEquals(0, response.length, "Не получен номер ФД");
+        Assertions.assertNotNull(response[3], "Не получен номер ФД");
+        Assertions.assertNotNull(response[4], "Не получена ФП");
+        Assertions.assertNotNull(response[5], "Не получена номер смены");
+        Assertions.assertNotNull(response[6], "Не получен номер документа в смене");
+        Assertions.assertNotNull(response[7], "Не получен дата документа");
+        Assertions.assertNotNull(response[8], "Не получен время документа");
+        String date = (String) response[7];
+        String time = (String) response[8];
+        LocalDateTime regDate = LocalDateTime.parse(date + time, DateTimeFormatter.ofPattern("ddMMyyHHmmss"));
+        Assertions.assertTrue(ChronoUnit.MINUTES.between(regDate, LocalDateTime.now()) < 5);
+    }
+}
